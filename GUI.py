d5645bfb5e91c854d55634f923e3a2ddf7f8cca0
Ramona Neher <ramona.j.neher@stud.hs-kempten.de>
2021-11-01 13:45:38 +0100

    Text field, exit-button and design updated

--- GUI.py
+++ GUI.py
@@ -32,21 +32,21 @@ headline.config(font=headline_font)
 
 input_field = tkinter.Entry(fenster, textvariable=user_input, bd=3, width=50)
 
-output_text.set("intercalactic currency in arabic number")
+output_text.set("intercalary currency in arabic numeral")
 output_field = tkinter.Label(fenster, textvariable=output_text)
 output_field.config(font=output_font)
 
 convert_button = tkinter.Button(fenster, text="convert", command=updated_output)
 convert_button.config(font=button_font)
 
-exit_button = tkinter.Button(fenster, text="Beenden", command=fenster.destroy)
+exit_button = tkinter.Button(fenster, text="Exit", command=fenster.destroy)
 exit_button.config(font=button_font)
 
 
 headline.place(x=500, y=30, anchor="center")
-input_field.place(x=350, y=100, anchor="center")
+input_field.place(x=250, y=100, anchor="center")
 output_field.place(x=750, y=100, anchor="center")
-convert_button.place(x=550, y=100, anchor="center")
+convert_button.place(x=500, y=100, anchor="center")
 exit_button.place(x=920, y=220, anchor="center")
 
 
